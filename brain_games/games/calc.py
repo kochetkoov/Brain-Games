ca8a93d0eae4b5f42357a0fe560257778d13357a
Алексей <kochetkoov.ao@mail.ru>
2024-07-14 17:55:25 +0400

    Stage 6

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,4 +1,5 @@
 import prompt
+import random
 
 
 def play_calc_game():
@@ -6,3 +7,34 @@ def play_calc_game():
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}')
     print('What is the result of the expression?')
+
+    correct_answers = 0
+    operations = ['+', '-', '*']
+
+
+    while correct_answers < 3:
+        random1 = random.randint(1, 100)
+        random2 = random.randint(1, 100)
+        operation = random.choice(operations)
+        if operation == '+':
+            correct_answer = random1 + random2
+        elif operation == '-':
+            correct_answer = random1 - random2
+        else:
+            correct_answer = random1 * random2
+
+
+        print(f'Question: {random1} {operation} {random2}')
+        answer = prompt.string('Your answer: ')
+        if answer == str(correct_answer):
+            print('Correct!')
+            correct_answers += 1
+        else:
+            print(
+                f"'{answer}' is wrong answer ;(."
+                f"Correct answer was '{correct_answer}'")
+            print(f"Let's try again, {name}!")
+            return
+
+
+    print(f'Congratulations, {name}!')
