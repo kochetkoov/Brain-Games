23304a3173c117f467f80794daff47fe75cb4994
Алексей <kochetkoov.ao@mail.ru>
2024-07-14 18:52:56 +0400

    Stage 7

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/kochetkoov/python-project-49/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/kochetkoov/python-project-49/actions)
-python-project-49/brain_calc_demo.cast
--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,8 +1,38 @@
 import prompt
 import random
+import math
 
 
-def play_calc_game():
+def play_gcd_game():
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}')
+    print('Find the greatest common divisor of given numbers.')
+
+
+    correct_answers = 0
+
+
+    while correct_answers < 3:
+        random1 = random.randint(1, 100)
+        random2 = random.randint(1, 100)
+        print(f'Question: {random1} {random2}')
+        user_answer = prompt.string('Your answer: ')
+        correct_answer = math.gcd(random1, random2)
+        if user_answer == str(correct_answer):
+            print('Correct!')
+            correct_answers += 1
+        else:
+            print(
+                f"'{user_answer}' is wrong answer ;(."
+                f"Correct answer was '{correct_answer}'")
+            print(f"Let's try again, {name}")
+            return
+
+
+    print(f'Congratulations, {name}!')
+
+
+
+
+
