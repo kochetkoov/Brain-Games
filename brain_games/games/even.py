aa4e54c9c1d2ecd9232e5fe1f69b9e37f5d495db
Алексей <kochetkoov.ao@mail.ru>
2024-07-14 15:32:22 +0400

    Stage 5

---
+++ brain_games/games/even.py
@@ -0,0 +1,32 @@
+import prompt
+import random
+
+
+def bool_function(number):
+    return number % 2 == 0
+
+
+def play_even_game():
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+
+    correct_answers = 0
+    while correct_answers < 3:
+        number = random.randint(1, 100)
+        print(f'Question: {number}')
+        user_answer = prompt.string('Your answer: ')
+
+        correct_answer = 'yes' if  bool_function(number) else 'no'
+        if user_answer == correct_answer:
+            print('Correct!')
+            correct_answers +=1
+        else:
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'")
+            print("Let's try again, Bill!")
+            return
+
+    print('Congratulations, Sam!')
+
+
---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from brain_games.games.even import play_even_game
+
+def main():
+    play_even_game()
+
+if __name__ == '__main__':
+    main()
