ae0347808de09011197823e31cddcc3fcc4e282e
Алексей <kochetkoov.ao@mail.ru>
2024-07-19 18:48:44 +0400

    update brain-gcd

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -31,7 +31,7 @@ def play_gcd_game():
             print(
                 f"'{user_answer}' is wrong answer ;(."
                 f"Correct answer was '{correct_answer}'")
-            print(f"Let's try again, {name}")
+            print(f"Let's try again, {name}!")
             return
 
     print(f'Congratulations, {name}!')
