d2edff2462fb1f8a67be1a751fe711c8f0eba86e
Алексей <kochetkoov.ao@mail.ru>
2024-07-27 16:04:33 +0400

    update brain_gcd

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -11,5 +11,5 @@ def generate_question_and_answer():
     random1 = random.randint(1, 100)
     random2 = random.randint(1, 100)
     correct_answer = gcd(random1, random2)
-    question = f'{random1}{random2}'
+    question = f'{random1} {random2}'
     return question, correct_answer
