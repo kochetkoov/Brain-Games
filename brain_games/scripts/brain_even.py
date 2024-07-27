bac21881a7db5f2eca4acb5057ef8af441b36dcf
Алексей <kochetkoov.ao@mail.ru>
2024-07-27 15:59:05 +0400

    update brain_even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -3,7 +3,7 @@
 from brain_games.engine import play_game
 from brain_games.games.even import generate_question_and_answer
 
-DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no"'
+DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def main():
